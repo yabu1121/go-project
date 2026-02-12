2dc1d1549bcfa49af7bd0971254654dcb6ec5fb1
yabu1121 <n.hayato0122@outlook.jp>
2026-02-12 18:58:35 +0900

    router

--- go-rest-api/router/router.go
+++ go-rest-api/router/router.go
@@ -2,14 +2,30 @@ package router
 
 import (
 	"go-rest-api/controller"
+	"os"
 
+	"github.com/labstack/echo-contrib/echojwt"
 	"github.com/labstack/echo/v4"
 )
 
-func NewRouter(uc controller.IUserController) *echo.Echo {
+func NewRouter(
+	uc controller.IUserController, 
+	tc controller.ITaskController
+) *echo.Echo {
 	e := echo.New()
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.Login)
 	e.POST("/logout", uc.Logout)
+
+	t := e.Group("/tasks")
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey: []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token",
+	}))
+	t.GET("", tc.GetAllTasks)
+	t.GET("/:taskId", tc.GetTaskById)
+	t.POST("", tc.CreateTask)
+	t.PUT("/:taskId", tc.UpdateTask)
+	t.DELETE("/:taskId", tc.DeleteTask)
 	return e
 }
